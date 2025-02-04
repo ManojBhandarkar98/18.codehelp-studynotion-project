506e956dc74c20c3b36f3a185d70f9480e61eb90
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-02-04 08:53:25 +0530

    Episode - 03 - Model - CourseController - createCourse handler function - validation for all field req, new entry in DB

---
+++ Server/controllers/Course.js
@@ -0,0 +1,51 @@
+const Course = require("../models/Course");
+const Tag = require("../models/tags");
+const User = require("../models/User");
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
+//createCourse handler function
+exports.createCourse = async (req, res) => {
+    try {
+
+        //fetch data 
+        const {courseName, courseDescription, whatYoutWillLearn, price, tag} = req.body;
+
+        //get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        //validation
+        if(!courseName || !courseDescription || !whatYoutWillLearn || !price || !tag || !thumbnail) {
+            return res.status(400).json({
+                success:false,
+                message:'All fields are required',
+            });
+        }
+        //create an entry for new Course
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn: whatYoutWillLearn,
+            price,
+            tag:tagDetails._id,
+            thumbnail:thumbnailImage.secure_url,
+        })
+
+         //return response
+        return res.status(200).json({
+            success:true,
+            message:"Course Created Successfully",
+            data:newCourse,
+        });
+
+    }
+    catch(error) {
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:'Failed to create Course',
+            error: error.message,
+        })
+    }
+};
+
