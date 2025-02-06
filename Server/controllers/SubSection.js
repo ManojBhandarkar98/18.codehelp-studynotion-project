a75eebcd6e1e6a7e407f2716cb69b703f307a069
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-02-06 21:01:11 +0530

    Episode - 04 - Model - SubSectionController - createSection handler function - upload to cloudinary and update section with this sub section ObjectId

--- Server/controllers/SubSection.js
+++ Server/controllers/SubSection.js
@@ -17,13 +17,22 @@ exports.createSubSection = async (req, res) => {
                     message:'All fields are required',
                 });
             }
-             //create a sub-section
+            //upload video to cloudinary
+            const uploadDetails = await uploadImageToCloudinary(video, process.env.FOLDER_NAME);
+            //create a sub-section
             const subSectionDetails = await SubSection.create({
                 title:title,
                 timeDuration:timeDuration,
                 description:description,
                 videoUrl:uploadDetails.secure_url,
             })
+            //update section with this sub section ObjectId
+            const updatedSection = await Section.findByIdAndUpdate({_id:sectionId},
+                                                        {$push:{
+                                                            subSection:subSectionDetails._id,
+                                                        }},
+                                                        {new:true});
+            //HW: log updated section here, after adding populate query
             //return response
             return res.status(200).json({
                 succcess:true,
